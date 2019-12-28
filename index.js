d717b37fed84809fac58af0c7695f9598f31ba42
Timo Deuschle <github@timodeuschle.de>
2019-12-28 16:20:46 +0100

    add custom header as allowed value

--- index.js
+++ index.js
@@ -10,19 +10,13 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
-
 app.all('/*', function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
 	res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
 	res.setHeader('Access-Control-Allow-Headers',
 		'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, ' +
-		'Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+		'Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-access-token'
 	);
 	next();
 });
