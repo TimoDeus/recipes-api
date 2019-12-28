2119cb768f95937475f4ccf4b5546c5d0e45f66b
Timo Deuschle <github@timodeuschle.de>
2019-12-28 16:14:18 +0100

    fix CORS issue

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.all('/*', function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
